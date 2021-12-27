84d322037932b4004fbbc07fa280d39fa37c34c5
Alessandro Sanino <alessandro@sanino.dev>
2021-12-27 23:54:17 +0100

    removed OVM networks and config
    Signed-off-by: Alessandro Sanino <alessandro@sanino.dev>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,6 @@
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
-import "@eth-optimism/plugins/hardhat/compiler";
 import "hardhat-gas-reporter";
 import "tsconfig-paths/register";
 
@@ -11,9 +10,9 @@ import { HardhatUserConfig } from "hardhat/config";
 
 import networks from "./hardhat.networks";
 
-const config : HardhatUserConfig = {
+const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.10",
+    version: "0.8.11",
     settings: {
       optimizer: {
         enabled: true,
@@ -24,9 +23,6 @@ const config : HardhatUserConfig = {
   gasReporter: {
     enabled: !!process.env.REPORT_GAS || false,
     coinmarketcap: process.env.COINMARKETCAP_API_KEY,
-  },
-  ovm: {
-    solcVersion: "0.8.10",
   },
   networks,
 };
